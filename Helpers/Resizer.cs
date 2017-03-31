94c73ca7dd6e34f5eba9a416f6118a6f4640a81b
mikettigit <acepost@protonmail.ch>
2017-03-31 15:59:44 +0300

    + ImageResizer helper

---
+++ Helpers/Resizer.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+
+namespace MvcApplication10.Helpers
+{
+    public static class Resizer
+    {
+        private static Image ScaleImage(Image source, int width, int height)
+        {
+
+            Image dest = new Bitmap(width, height);
+            using (Graphics gr = Graphics.FromImage(dest))
+            {
+                gr.FillRectangle(Brushes.White, 0, 0, width, height);  // Очищаем экран
+                gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                float srcwidth = source.Width;
+                float srcheight = source.Height;
+                float dstwidth = width;
+                float dstheight = height;
+
+                if (srcwidth <= dstwidth && srcheight <= dstheight)  // Исходное изображение меньше целевого
+                {
+                    int left = (width - source.Width) / 2;
+                    int top = (height - source.Height) / 2;
+                    gr.DrawImage(source, left, top, source.Width, source.Height);
+                }
+                else if (srcwidth / srcheight > dstwidth / dstheight)  // Пропорции исходного изображения более широкие
+                {
+                    float cy = srcheight / srcwidth * dstwidth;
+                    float top = ((float)dstheight - cy) / 2.0f;
+                    if (top < 1.0f) top = 0;
+                    gr.DrawImage(source, 0, top, dstwidth, cy);
+                }
+                else  // Пропорции исходного изображения более узкие
+                {
+                    float cx = srcwidth / srcheight * dstheight;
+                    float left = ((float)dstwidth - cx) / 2.0f;
+                    if (left < 1.0f) left = 0;
+                    gr.DrawImage(source, left, 0, cx, dstheight);
+                }
+
+                return dest;
+            }
+        }
+    }
+}
