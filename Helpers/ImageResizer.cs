b21562e651a406401483062865226e089d6f5a85
mikettigit <acepost@protonmail.ch>
2017-04-10 11:23:18 +0300

    ImageResizer Helper

--- Helpers/ImageResizer.cs
+++ Helpers/ImageResizer.cs
@@ -1,9 +1,45 @@
 ﻿using System.Drawing;
+using System.IO;
 
 namespace MvcApplication10.Helpers
 {
-    public static class Resizer
+    public static class ImageResizer
     {
+        public static void Proceed43(string filename, string newfilename, int DefaultWidth = 650)
+        {
+            FileInfo file = new FileInfo(filename);
+            System.Drawing.Image img = null;
+            System.Drawing.Image objImage = System.Drawing.Image.FromFile(filename);
+
+            int w = (int)((float)objImage.Height * 4 / 3);
+            if (w > objImage.Width)
+            {
+                img = ScaleImage(objImage, w, objImage.Height);
+                objImage.Dispose();
+                img.Save(newfilename);
+                objImage = img;
+            }
+            if (objImage.Width > DefaultWidth)
+            {
+                int h = (int)((float)objImage.Height * DefaultWidth / (float)objImage.Width);
+                img = ScaleImage(objImage, DefaultWidth, h);
+                objImage.Dispose();
+                img.Save(newfilename);
+                objImage = img;
+            }
+        }
+
+        public static void Proceed(string filename, string newfilename, int width, int height)
+        {
+            FileInfo file = new FileInfo(filename);
+            System.Drawing.Image img = null;
+            System.Drawing.Image objImage = System.Drawing.Image.FromFile(filename);
+
+            img = ScaleImage(objImage, width, height);
+
+            img.Save(newfilename);
+        }
+
         private static Image ScaleImage(Image source, int width, int height)
         {
 
